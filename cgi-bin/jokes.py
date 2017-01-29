6be4163e061afe983e447bddc493a7de65fff879
mia0223 <wangchenyang0223@gmail.com>
2017-01-29 00:27:37 -0500

    Update jokes.py

--- cgi-bin/jokes.py
+++ cgi-bin/jokes.py
@@ -1,22 +1,37 @@
-import urllib.request
-import urllib.parse
+import urllib
 import bs4 as bs
 import re
 from random import randint
-
-
-url="http://worldsfunniestjokes.club/top-50-funniest-jokes-in-the-world/"
-
-subcontent = urllib.request.urlopen(url).read()
-subsoup=bs.BeautifulSoup(subcontent,'html.parser')
-theBody=re.findall(re.compile("<br(.*?)</p>",re.DOTALL),str(subsoup))
-
-randomInt=randint(1,len(theBody)-1)
-s=theBody[randomInt][3:]
-Joke={
-    'joke':s
-}
-
 import json
-print(json.dumps(Joke))
+
+jsonArr=[]
+
+def repeat():
+	for x in range(0,5):
+		url="http://worldsfunniestjokes.club/top-50-funniest-jokes-in-the-world/"
+
+		subcontent = urllib.urlopen(url).read()
+		subsoup=bs.BeautifulSoup(subcontent,'html.parser')
+		theBody=re.findall(re.compile("<br(.*?)</p>",re.DOTALL),str(subsoup))
+
+		randomInt=randint(1,len(theBody)-1)
+		s=theBody[randomInt][3:]
+		
+		Joke={'joke':s}
+
+		print(json.dumps(Joke))
+
+		jsonArr.extend([json.dumps(Joke)])
+
+	return jsonArr
+		
+
+repeat()
+Json = {'jokes': jsonArr}
+
+f = open('jokes.json', 'a')
+f.write(json.dumps(Json))
+f.write('\n')
+f.close
+
 
