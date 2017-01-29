83e8179e51481a2fd5992a991ea2c1db9ff91ddd
mia0223 <wangchenyang0223@gmail.com>
2017-01-29 00:28:45 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,40 +9,60 @@ category = ['art', 'biography', 'business', 'chick-lit', 'children-s', 'christia
 			'manga', 'memoir', 'music', 'mystery', 'nonfiction', 'paranormal', 'philosophy', 'poetry', 'psychology', 'religion',
 			'romance', 'science', 'science-fiction', 'self-help', 'suspense', 'spirituality', 'sports', 'thriller', 'travel']
 
-url = "https://www.goodreads.com/genres/" + random.choice(category)
+jsonArr=[]
+def repeat():
+	for x in range(0,5):
+		url = "https://www.goodreads.com/genres/" + random.choice(category)
 
-source = urllib.urlopen(url).read()
-soup = BeautifulSoup(source,'html.parser')
+		source = urllib.urlopen(url).read()
+		soup = BeautifulSoup(source,'html.parser')
 
-temp = soup.findAll('div', {'class': 'coverWrapper'})
-randomNum = random.randint(0, len(temp)-1)
+		temp = soup.findAll('div', {'class': 'coverWrapper'})
+		randomNum = random.randint(0, len(temp)-1)
 
-'Get Book Name && Book Cover'
-oneDiv = temp[randomNum].findAll('img')[0]
-title = oneDiv.get('alt')
-imgLink = oneDiv.get('src')
+		'Get Book Name && Book Cover'
+		oneDiv = temp[randomNum].findAll('img')[0]
+		title = oneDiv.get('alt')
+		imgLink = oneDiv.get('src')
 
-'Get Book ID'
-bookURL = "https://www.goodreads.com/" + temp[randomNum].findAll('a')[0].get('href')
+		'Get Book ID'
+		bookURL = "https://www.goodreads.com/" + temp[randomNum].findAll('a')[0].get('href')
 
-subSource = urllib.urlopen(bookURL).read()
-subSoup = BeautifulSoup(subSource, 'html.parser')
+		subSource = urllib.urlopen(bookURL).read()
+		subSoup = BeautifulSoup(subSource, 'html.parser')
 
-for bookId in subSoup.findAll('a'):
-	if bookId.get('data-text-id') != None:
-		break
+		for bookId in subSoup.findAll('a'):
+			if bookId.get('data-text-id') != None:
+				break
 
-bookId = "freeTextContainer" + bookId.get('data-text-id').encode('ascii', 'ignore')
+		bookId = "freeTextContainer" + bookId.get('data-text-id').encode('ascii', 'ignore')
 
-description = subSoup.findAll('span', {'id': bookId})[0].text
+		description = subSoup.findAll('span', {'id': bookId})[0].text
 
-final = {"title": title}
-final["image"] = imgLink
-final["url"] = bookURL
-final["description"] = description
+		final = {"title": title}
+		final["image"] = imgLink
+		final["url"] = bookURL
+		final["description"] = description
 
 
-print json.dumps(final)
+		print json.dumps(final)
+		jsonArr.extend([json.dumps(final)])
+
+	return jsonArr
+
+repeat()
+Json = {'books': jsonArr}
+
+f = open('books.json', 'a')
+f.write(json.dumps(Json))
+f.write('\n')
+f.close
+
+
+
+
+
+
 
 
 
