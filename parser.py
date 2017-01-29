647929ca4b8c71de776ff086f6db79a64955c59a
mia0223 <wangchenyang0223@gmail.com>
2017-01-28 22:40:55 -0500

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import urllib
+import random
+import re
+import json
+
+category = ['art', 'biography', 'business', 'chick-lit', 'children-s', 'christian', 'classics', 'comics', 'contemporary', 'cookbooks',
+			'crime', 'fantasy', 'fiction', 'gay-and-lesbian', 'graphic-novels', 'historical-fiction', 'history', 'horror', 'humor-and-comedy',
+			'manga', 'memoir', 'music', 'mystery', 'nonfiction', 'paranormal', 'philosophy', 'poetry', 'psychology', 'religion',
+			'romance', 'science', 'science-fiction', 'self-help', 'suspense', 'spirituality', 'sports', 'thriller', 'travel']
+
+url = "https://www.goodreads.com/genres/" + random.choice(category)
+
+source = urllib.urlopen(url).read()
+soup = BeautifulSoup(source,'html.parser')
+
+temp = soup.findAll('div', {'class': 'coverWrapper'})
+randomNum = random.randint(0, len(temp)-1)
+
+'Get Book Name && Book Cover'
+oneDiv = temp[randomNum].findAll('img')[0]
+title = oneDiv.get('alt')
+imgLink = oneDiv.get('src')
+
+'Get Book ID'
+bookURL = "https://www.goodreads.com/" + temp[randomNum].findAll('a')[0].get('href')
+
+subSource = urllib.urlopen(bookURL).read()
+subSoup = BeautifulSoup(subSource, 'html.parser')
+
+for bookId in subSoup.findAll('a'):
+	if bookId.get('data-text-id') != None:
+		break
+
+bookId = "freeTextContainer" + bookId.get('data-text-id').encode('ascii', 'ignore')
+
+description = subSoup.findAll('span', {'id': bookId})[0].text
+
+final = {"title": title}
+final["image"] = imgLink
+final["url"] = bookURL
+final["description"] = description
+
+
+print json.dumps(final)
+
+
+
+
+
+
+
+
+
