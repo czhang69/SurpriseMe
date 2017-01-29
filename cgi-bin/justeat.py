436613eccb20b1be76922e951e88f384218c532e
mia0223 <wangchenyang0223@gmail.com>
2017-01-29 00:28:16 -0500

    Update justeat.py

--- cgi-bin/justeat.py
+++ cgi-bin/justeat.py
@@ -3,18 +3,35 @@ import bs4 as bs
 import random
 import json
 
-sauce = urllib.urlopen("https://www.just-eat.ca/area/h3a-montreal/").read()
-soup = bs.BeautifulSoup(sauce,"lxml")
+jsonArr=[]
+def repeat():
+	for x in range(0,5):
+		sauce = urllib.urlopen("https://www.just-eat.ca/area/h3a-montreal/").read()
+		soup = bs.BeautifulSoup(sauce,"html.parser")
+		
+		num = random.randint(0, len(soup.find_all("h3"))-1)
+		res = soup.find_all("h3")[num]
 
-res = random.sample(soup.find_all("h3"),1)
+		info = {"name":res.text}
 
-info = {"name":res[0].text}
+		addr = res.parent.parent.find_all("p",itemprop="address")
+		info["address"] = addr[0].text
 
-addr = res[0].parent.parent.find_all("p",itemprop="address")
-info["address"] = addr[0].text
+		link = res.parent.parent.parent.parent.parent.parent.find_all("a",{'class':"mediaElement listing-item-link"})
+		web = "https://www.just-eat.ca"+link[0]['href']
+		info["website"] = web
 
-link = res[0].parent.parent.parent.parent.parent.parent.find_all("a",{'class':"mediaElement listing-item-link"})
-web = "https://www.just-eat.ca"+link[0]['href']
-info["website"] = web
+		print json.dumps(info)
 
-print json.dumps(info)
+		jsonArr.extend([json.dumps(info)])
+
+	return jsonArr
+
+
+repeat()
+Json = {'restaurant': jsonArr}
+
+f = open('restaurant.json', 'a')
+f.write(json.dumps(Json))
+f.write('\n')
+f.close
